3d3af1017c50f0befef11dcd73283906091d436d
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-23 23:05:29 +0300

    feat: add solution for cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import process from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  console.log(process.argv)
+  const cliArgs = process.argv.slice(2).reduce((args, value) => {
+    if (value.startsWith('--')) {
+      args.push([ null, null ])
+      args[args.length - 1][0] = value.slice(2);
+    } else if (args.length > 0) {
+      args[args.length - 1][1] = value;
+    }
+    return args;
+  }, [])
+  const cliArgsFormated = cliArgs.map(([ key, value ]) => `${key} is ${value}`)
+  console.log(cliArgsFormated.join(', '))
 };
 
 parseArgs();
