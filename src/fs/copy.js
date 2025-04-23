d80419e4d99797ce5c651fd545cc65ab1373edbf
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-23 17:40:29 +0300

    feat: add solution for copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from "node:fs/promises"
+import path from "node:path"
+
 const copy = async () => {
-    // Write your code here 
+  const pathOrigin = path.join('src', 'fs', 'files');
+  const pathTarget = path.join('src', 'fs', 'files_copy');
+  const failedMessage = "FS operation failed";
+
+  try {
+    const files = await fs.readdir(pathOrigin);
+    await fs.mkdir(pathTarget);
+    files.map((file) => {
+      const fileName = path.join(pathOrigin, file);
+      const dest = path.join(pathTarget, file);
+      fs.copyFile(fileName, dest);
+    })
+  } catch (err) {
+    if (err.code === "EEXIST" || err.code === "ENOENT") {
+      throw new Error(failedMessage)
+    } else {
+      throw err;
+    }
+  }
 };
 
 await copy();
