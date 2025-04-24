651a3277279678eb66140a2f31b55143b5c04296
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-24 19:37:28 +0300

    feat: add soultion for streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import {createReadStream} from "node:fs";
+import path from "node:path";
+import process from "node:process"
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join('src', 'streams', 'files', 'fileToRead.txt');
+  const readStream = createReadStream(filePath);
+  readStream.on("data", (chunk) => {process.stdout.write(chunk)})
+  readStream.on("end", () => {process.stdout.write("\n")})
 };
 
 await read();
