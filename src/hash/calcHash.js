c07125dc6ea62b984f0e54e6342c1a07a195765e
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-26 00:47:40 +0300

    feat: add soultion for hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from "node:crypto"
+import { createReadStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import process from "node:process"
+import path from "node:path"
+
 const calculateHash = async () => {
-    // Write your code here 
+  const secret = 'super-secret'
+  const filePath = path.join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+  const readStream = createReadStream(filePath);
+  const hmac = createHash('sha256', secret);
+  await pipeline(readStream, hmac);
+  process.stdout.write(hmac.digest('hex'));
+  process.stdout.write('\n');
 };
 
 await calculateHash();
