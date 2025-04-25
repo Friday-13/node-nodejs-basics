6b1601b186267f1a0fb2073742962f395eacbc5d
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-26 00:13:37 +0300

    feat: add soultion for child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import {fork} from "node:child_process"
+import path from "node:path";
+import process from "node:process"
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.join('src', 'cp', 'files', 'script.js')
+  const childProcess = fork(scriptPath, args);
+  process.on('message', (message) => childProcess.stdin(message))
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hehe', 'haha']);
