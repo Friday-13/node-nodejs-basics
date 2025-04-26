a43cecb4b06654dd1178d72dd991fa0228e7aa65
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-04-26 23:38:10 +0300

    fix: change output format for cli env

--- src/cli/env.js
+++ src/cli/env.js
@@ -4,8 +4,8 @@ const parseEnv = () => {
   const prefix = 'RSS_';
   const envPairs = Object.entries(process.env)
   const rssEnvs = envPairs.filter(([key]) => key.startsWith(prefix))
-  const rssEnvFormatedStrings = rssEnvs.map(([key, value]) => `${key}=${value};`)
-  const output = rssEnvFormatedStrings.join(' ');
+  const rssEnvFormatedStrings = rssEnvs.map(([key, value]) => `${key}=${value}`)
+  const output = rssEnvFormatedStrings.join('; ');
   console.log(output)
 };
 
